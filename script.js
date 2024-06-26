5af1cd3b9b18971bc0f805518052695bf074a881
Ruby Casey <ruby@cxsey.com>
2024-06-26 13:58:53 -0400

    Opacity Change

--- script.js
+++ script.js
@@ -11,14 +11,27 @@ console.log(containerSize)
 createNewGrid();
 
 function createNewGrid() {
+    // Container Math
     const gridSize = gridSideSize ** 2;
     let gridCellSize = containerSize / gridSideSize;
     container.style.width = containerSizePx
+
     for (i = 0; i != gridSize; i++){
+        // Create Cell
         const gridCell = document.createElement("div");
         gridCell.classList.add("gridCell");
         gridCell.style.height = gridCellSize + "px";
         gridCell.style.width = gridCellSize + "px";
+        gridCell.style.opacity = 0;
+
+        // Cell Functions
+        gridCell.addEventListener("mouseenter", () => {
+            gridCell.style.opacity = Number(gridCell.style.opacity) + 0.1;
+        });
+        //gridCell.addEventListener("mouseleave", () => {
+        //    gridCell.style.opacity = Number(gridCell.style.opacity) + 0.1;
+        //});
+
         container.appendChild(gridCell);
         //console.log("Cell Created");
     }
