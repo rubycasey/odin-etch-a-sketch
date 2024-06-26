6473a41e881cc6c94e18b684f7b3252097098df9
Ruby Casey <ruby@cxsey.com>
2024-06-26 13:17:53 -0400

    Gird Creation

--- script.js
+++ script.js
@@ -2,18 +2,24 @@
 let gridSideSize = 16;
 
 // Document Elements
-const container = document.getElementById("container");
+const container = document.getElementById("grid-container");
+const containerSizePx = window.getComputedStyle(container).getPropertyValue('height');
+const containerSize = containerSizePx.slice(0, containerSizePx.length-2);
 
 // Creating divs
-
+console.log(containerSize)
 createNewGrid();
 
 function createNewGrid() {
-    const gridSize = gridSideSize ** 2
+    const gridSize = gridSideSize ** 2;
+    let gridCellSize = containerSize / gridSideSize;
+    container.style.width = containerSizePx
     for (i = 0; i != gridSize; i++){
         const gridCell = document.createElement("div");
         gridCell.classList.add("gridCell");
+        gridCell.style.height = gridCellSize + "px";
+        gridCell.style.width = gridCellSize + "px";
         container.appendChild(gridCell);
-        console.log("Cell Created");
+        //console.log("Cell Created");
     }
 };
